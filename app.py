c986a24e038b09c4a2078f4ef7f7ac6825344c0e
khalifa352 <khalifawelbaz@gmail.com>
2026-02-21 16:32:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def generate():
         logger.info("🛰️ calling Gemini now...")
 
         response = client.models.generate_content(
-            model="gemini-1.5-pro", 
+            model="gemini-2.0-flash", 
             contents=contents,
             config=types.GenerateContentConfig(system_instruction=system_instruction, temperature=0.2)
         )
